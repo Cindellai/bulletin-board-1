f95694bcc9ef24f36aff3e9733d18857b9762ab7
cindellai <cindellai26@gmail.com>
2024-06-23 12:52:47 -0500

    finished code

---
+++ app/models/board.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Board < ApplicationRecord
+  def posts
+    return Post.where({ :board_id => self.id })
+  end
+end
